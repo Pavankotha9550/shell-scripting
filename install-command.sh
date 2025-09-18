d1e8bf95573a5a5100992e6eca813b5f30362314
pavankotha <pavankotha9550@outlook.com>
2025-09-18 16:53:38 +0530

    installation file updated created testing now

--- install-command.sh
+++ install-command.sh
@@ -6,4 +6,15 @@ echo "userid:$userid"
 if [ $userid -ne 0 ] 
 then
     echo "error: not sudo user"
+    exit 1
+fi
+
+dnf install mysql -y
+
+fi [ $? -eq 0 ]
+then
+    echo "installation is success"
+else
+    echo "error: installation failed"
+    exit 1
 fi
