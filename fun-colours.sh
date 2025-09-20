bd2752faac78e91ef48042df2c98ff99f643f059
pavankotha <pavankotha9550@outlook.com>
2025-09-20 07:48:40 +0530

    fun-color file updated created testing now

--- fun-colours.sh
+++ fun-colours.sh
@@ -10,47 +10,47 @@ W="\e[0m"
 echo "userid:$userid"
 if [ $userid -ne 0 ] 
 then
-    echo "$R error: not sudo user $W"
+    echo -e "$R error: not sudo user $W"
     exit 1
 else
-    echo "$G running with sudo user $W"
+    echo -e "$G running with sudo user $W"
 fi
 VALIDATE()
 {
      if [ $1 -eq 0 ]
     then
-        echo "$G installation $2 is success $W"
+        echo -e "$G installation $2 is success $W"
     else
-        echo "$R error: installation $2 failed $W"
+        echo -e "$R error: installation $2 failed $W"
         exit 1
     fi
 }
 dnf list installed mysql
 if [ $? -ne 0 ]
 then
-    echo "not installed mysql going to install now"
+    echo -e "not installed mysql going to install now"
     dnf install mysql -y
     VALIDATE $? mysql
 else
-    echo "$G already mysql installed $W"
+    echo -e "$G already mysql installed $W"
 fi
 
 dnf list installed python3
 if [ $? -ne 0 ]
 then
-    echo "not installed python3 going to install now"
+    echo -e "not installed python3 going to install now"
     dnf install python3 -y
     VALIDATE $? python3
 else
-    echo "$G already python3 installed $W"
+    echo -e "$G already python3 installed $W"
 fi
 
 dnf list installed nginx
 if [ $? -ne 0 ]
 then
-    echo "not installed nginx going to install now"
+    echo -e "not installed nginx going to install now"
     dnf install nginx -y
     VALIDATE $? nginx
 else
-    echo "$G already nginx installed $W"
+    echo -e "$G already nginx installed $W"
 fi
