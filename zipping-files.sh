4830c649e917808575eea972082f8b4df7316f39
pavankotha <pavankotha9550@outlook.com>
2025-10-02 09:22:21 +0530

    modified scrits

--- zipping-files.sh
+++ zipping-files.sh
@@ -7,5 +7,5 @@ days=$1
 
 files=$(find $source_dir -name "*.log" -mtime +$days)
 
-zip_file="$dest_dir/$(date +%F-%h-%m-%s).zip"
-find $source_dir -name "*.log" -mtime +$days | zip -@ "$zip_file"
+zip_file=$dest_dir/$(date +%F-%h-%m-%s).zip
+find $source_dir -name "*.log" -mtime +$days | zip -@ $zip_file
