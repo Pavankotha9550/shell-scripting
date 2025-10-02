0753bc8f6278333aec6671a7979594b02cf18de3
pavankotha <pavankotha9550@outlook.com>
2025-10-02 09:19:23 +0530

    modified scrits

--- zipping-files.sh
+++ zipping-files.sh
@@ -5,7 +5,7 @@ mkdir -p destination
 dest_dir="/home/ec2-user/destination"
 days=$1
 
-files=$(find $source_dir -name "*.log" -mtime $days)
+files=$(find $source_dir -name "*.log" -mtime +$days)
 
 zip_file="$dest_dir/$(date +%F-%h-%m-%s).zip"
-find $source_dir -name "*.log" -mtime $days | zip -@ "$zip_file"
+find $source_dir -name "*.log" -mtime +$days | zip -@ "$zip_file"
