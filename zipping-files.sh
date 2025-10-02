508e39df9f3b59317ac34838a2560bca50ac567c
pavankotha <pavankotha9550@outlook.com>
2025-10-02 10:15:30 +0530

    modified scrits

--- zipping-files.sh
+++ zipping-files.sh
@@ -7,12 +7,25 @@ failure()
 }
 
 trap 'failure "${LINENO}" "${BASH_COMMAND}"' ERR
+
 source_dir="/home/ec2-user"
 mkdir -p destination
 dest_dir="/home/ec2-user/destination"
 days=$1
 
 files=$(find $source_dir -name "*.log" -mtime +$days)
+if [ -n ""files ]
+    then
+        echo "files we got from the find are:"
+        while IFS= read -r filename
+        do
+            echo $filename
+        done <<<$files
+    else
+        echo "no file are there for $days days"
+        exit 0
+
+fi
 
 zip_file="$dest_dir/$(date +%F-%H-%M-%S).zip"
 find "$source_dir" -name "*.log" -mtime +$days | zip -@ "$zip_file"
